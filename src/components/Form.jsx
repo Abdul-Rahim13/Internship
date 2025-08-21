bf5b54b483a8161ab168431e5e62ef9c74a83d59
Abdul Rahim <raheemabdur106@gmail.com>
2025-08-21 13:40:33 +0500

    forms

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 
 function Form() {
+    const [isEditButton, setEditButton] = useState(false)
+    const [editindex, setEditindex] = useState(null)
     const [data, setData] = useState([])
     const [form, setForm] = useState({
         name: '',
-        number: ' ',
-        email: ' ',
-        city: ' '
+        number: '',
+        email: '',
+        city: ''
     })
 
     const addData = (e) => {
@@ -15,22 +17,81 @@ function Form() {
             alert("Please Enter All Fields")
             return
         }
-        setData([...data, form])
+
+        if(!form.name){
+            alert("Name is Empty")
+            return
+        }else if(!form.number){
+            alert("Number is Empty")
+            return
+        }else if(!form.email){
+            alert("Email is Empty")
+            return
+        }else if(!form.city){
+            alert("CIty is Empty")
+            return
+        }
+
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        if(!emailPattern.test(form.email)){
+            alert("please Enter a valid Email Address")
+            return
+        }
+
+        if(isEditButton && editindex !== null){
+            const updateData = [...data]
+            updateData[editindex] = form
+            setData(updateData)
+            setEditButton(false)
+            setEditindex(null)
+        }else{
+            setData([...data, form])
+        }
+        
         setForm({name: '',number: '',email: '',city: ''})
     }
 
     
     const handleInputs = (e) => {
         
-      const  name = e.target.name
-       const value = e.target.value
+        const  name = e.target.name
+        const value = e.target.value
 
-        setForm({...form, [name]:value})
+        if(name === "number"){
+            if(/^[0-9\b]+$/.test(value)){
+                setForm({...form, [name]:value})
+            }
+        }else{
+            setForm({...form, [name]:value})
+        }
     }
 
+    const handleEdit = (index) => {
+        setEditButton(true)
+        setEditindex(index)
+        const selectedData = data[index]
+        if (selectedData){
+            setForm({
+                name: selectedData.name,
+                number: selectedData.number,
+                email: selectedData.email,
+                city: selectedData.city
+            })
+        }
+    }
+
+    const handleDelete = (index) => {
+        const act_index = index - 1
+        if(act_index >=0 ){
+            const updateData = data.filter((item, idx) => idx !== act_index)
+            setData(updateData)
+        }
+    }
   return (
-    <div className='flex flex-col justify-center items-center gap-y-5 mt-15'>
-        <form className='justify-center items-center'>
+    
+    <div className='grid grid-cols-2 justify-center items-center gap-y-5 mt-15 ml-[-250px]'>
+        <div className='flex justify-center items-center'>
+            <form className='justify-center items-center'>
 
             <h1 className='mb-2'>Name:</h1>
             <input className='border-1 border-gray-700 w-55 pl-2 text-[12px]' type="text" name='name'  placeholder='Enter you name' 
@@ -53,20 +114,46 @@ function Form() {
             onChange={handleInputs}/>
 
             <div className='flex flex-col justify-center items-center mt-10'>
-                <button className='cursor-pointer h-10 w-25 bg-amber-100 rounded-2xl ' onClick={addData}>Add Data</button>
+                <button className='cursor-pointer h-10 w-25 bg-amber-100 rounded-2xl ' onClick={addData}>{isEditButton ? "Edit data": "Save Data"}</button>
             </div>
 
         </form>
+        </div>
 
-        <div>
-            <h1 className='font-bold'>Data Stored : </h1>
-            <ol>
-                {
-                    data.map((item) => (
-                        <li>Name: {item.name} - Phone No: {item.number} - Email:  {item.email} - City: {item.city}</li>
-                    ))
-                }
-            </ol>
+        <div className='flex mb-50 h-24 flex-col w-[800px]'>
+            <h1 className='font-bold text-2xl'>Data Stored : </h1>
+                <table >
+                    <thead className='text-center font-semibold text-[18px]'>
+                        <tr>
+                            <td>Sr No:</td>
+                            <td>Name</td>
+                            <td>Number</td>
+                            <td>Email</td>
+                            <td>City</td>
+                            <td>Actions</td>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {
+                            data.map((item, index) => {
+                                return (
+                                    <tr key={index} className='text-center'>
+                                        <td>{index+1}</td>
+                                        <td>{item.name}</td>
+                                        <td>{item.number}</td>
+                                        <td>{item.email}</td>
+                                        <td>{item.city}</td>
+                                        <td className='flex gap-5 mb-2 justify-center'>
+                                            <button className='cursor-pointer h-8 w-25 bg-blue-400 rounded-2xl' onClick={ () => handleEdit(index)}>Edit</button>
+                                            <button className='cursor-pointer h-8 w-25 bg-red-500 rounded-2xl'  onClick={ () => handleDelete(index)}>Delete</button>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
         </div>
 
 
