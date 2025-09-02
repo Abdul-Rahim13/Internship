32c4ee2f37de73320f2f8892983d819634ae9e62
Abdul Rahim <raheemabdur106@gmail.com>
2025-09-02 22:53:14 +0500

    Axios

--- src/components/fetchapi/Api.jsx
+++ src/components/fetchapi/Api.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 function Api() {
+    const [data, setData] = useState([])
+    const API = 'https://fakestoreapi.com/products'
+    // Handle HTTP req and responses
+     const fectData = async () => {
+        try {
+            const res = await axios.get(API)
+            console.log(res.data)
+            setData(res.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fectData()
+    },[])
+
   return (
-    <div>Api</div>
+    <div className="grid grid-cols-4 gap-25 mt-5 ml-5 mr-5">
+        {
+            data.map((item) => (
+                <li key={item.id} className="list-none border p-2 rounded shadow">
+                    <h1 className="font-bold text-sm mb-2">{item.title}</h1>
+                    <img src={item.image} alt={item.title} className="w-32 h-32 object-contain mx-auto"/>
+                    <p className="text-green-600 font-semibold">${item.price}</p>
+                    <p className="text-xs text-gray-600">{item.description}</p>
+                </li>
+            ))
+        }
+    </div>
   )
 }
 
