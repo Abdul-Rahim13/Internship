93f96e1c7c3a3c6d0548247b2c167a6723092c0c
Abdul Rahim <raheemabdur106@gmail.com>
2025-09-03 11:42:28 +0500

    API

--- src/components/fetchapi/Api.jsx
+++ src/components/fetchapi/Api.jsx
@@ -26,7 +26,7 @@ function Api() {
                 <li key={item.id} className="list-none border p-2 rounded shadow">
                     <h1 className="font-bold text-sm mb-2">{item.title}</h1>
                     <img src={item.image} alt={item.title} className="w-32 h-32 object-contain mx-auto"/>
-                    <p className="text-green-600 font-semibold">${item.price}</p>
+                    <p className="text-green-600 font-semibold">{item.price}</p>
                     <p className="text-xs text-gray-600">{item.description}</p>
                 </li>
             ))
