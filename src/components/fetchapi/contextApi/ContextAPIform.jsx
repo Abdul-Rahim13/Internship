42e9cf2391a4e1c5b11a3024da6c78668877e852
Abdul Rahim <raheemabdur106@gmail.com>
2025-09-04 11:42:24 +0500

    Context API Fetching

---
+++ src/components/fetchapi/contextApi/ContextAPIform.jsx
@@ -0,0 +1,17 @@
+
+
+
+export const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+
+    
+
+}
+
+
+return (
+    <AppContext.Provider value={{}}>
+      {children}
+    </AppContext.Provider>
+  );
