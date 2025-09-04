c0939f5f2f583a0cb65b1555db0d4fd053dcf25c
Abdul Rahim <raheemabdur106@gmail.com>
2025-09-04 12:19:49 +0500

    Lottie json loader and products page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import './App.css'
 import { Routes, Route } from "react-router-dom"
+import { AppProvider } from './components/fetchapi/contextApi/ContextAPIform'
 import Products from './components/fetchapi/Products'
-import ProductDetails from './components/fetchapi/ProductDetails'
-import Editproduct from './components/fetchapi/Editproduct'
-import Newproduct from './components/fetchapi/Newproduct'
+import ProductsDetails from './components/fetchapi/contextApi/ProductsDetails'
+import Newproduct from './components/fetchapi/contextApi/Newproduct'
+import Editproduct from './components/fetchapi/contextApi/Editproduct'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Products />} />
-      <Route path="/products/:id" element={<ProductDetails />} />
-      <Route path="/products/new" element={<Newproduct />} />
-      <Route path="/products/:id/edit" element={<Editproduct />} />
-    </Routes>
+    <AppProvider>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/products/:id" element={<ProductsDetails />} />
+        <Route path="/products/new" element={<Newproduct />} />
+        <Route path="/products/:id/edit" element={<Editproduct />} />
+      </Routes>
+    </AppProvider>
   )
 }
 
--- src/components/fetchapi/contextApi/ContextAPIform.jsx
+++ src/components/fetchapi/contextApi/ContextAPIform.jsx
@@ -1,17 +1,36 @@
-
+import axios from "axios";
+import { useEffect, useState, createContext } from "react";
+import { useParams } from "react-router-dom";
 
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false)
+  const { id } = useParams();
+  const API = "https://fakestoreapi.com/products";
 
-    
+  const fetchProducts = async () => {
+    try {
+    setLoading(true)
+      const res = await axios.get(API);
+      setData(res.data);
+    } catch (error) {
+      console.log(error);
+    }finally{
+        setLoading(false)
+    }
+  };
 
-}
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-
-return (
-    <AppContext.Provider value={{}}>
+  return (
+    <AppContext.Provider value={{ data, fetchProducts, loading }}>
       {children}
     </AppContext.Provider>
   );
+};
+
